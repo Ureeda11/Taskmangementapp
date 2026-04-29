8a53ffa3d2212354008686ee7886955e44f38e53
Ureeda11 <ureedabibi2006@gmail.com>
2026-04-29 23:52:56 +0500

    fix: set all api routes to force-dynamic

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -43,3 +43,4 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
+export const dynamic = 'force-dynamic';
--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,11 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('mongodb://ureead2006_db_user:helloworld@ac-m3nfm9a-shard-00-00.zwea1p8.mongodb.net:27017,ac-m3nfm9a-shard-00-01.zwea1p8.mongodb.net:27017,ac-m3nfm9a-shard-00-02.zwea1p8.mongodb.net:27017/?ssl=true&replicaSet=atlas-1gl7is-shard-0&authSource=admin&appName=Cluster0');
+  throw new Error("mongodb://ureead2006_db_user:helloworld@ac-m3nfm9a-shard-00-00.zwea1p8.mongodb.net:27017,ac-m3nfm9a-shard-00-01.zwea1p8.mongodb.net:27017,ac-m3nfm9a-shard-00-02.zwea1p8.mongodb.net:27017/?ssl=true&replicaSet=atlas-1gl7is-shard-0&authSource=admin&appName=Cluster0");
 }
-
 
 let cached = (global as any).mongoose;
 
@@ -13,31 +12,15 @@ if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
-await mongoose.connect(MONGODB_URI!); 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      console.log("✅ MongoDB Connected Successfully!");
+    cached.promise = mongoose.connect(MONGODB_URI!).then((mongoose) => {
       return mongoose;
     });
   }
-
-  try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    console.error("❌ MongoDB Connection Error:", e);
-    throw e;
-  }
-
+  cached.conn = await cached.promise;
   return cached.conn;
 }
 
